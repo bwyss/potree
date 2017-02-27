f61e1a8832781cfaeac84a41d326a7a967dc5c23
ben <bmwyss@gmail.com>
2017-02-27 14:01:11 -0800

    fix Uncaught ReferenceError: proj4 is not defined

--- build/potree/potree.js
+++ build/potree/potree.js
@@ -15696,7 +15696,7 @@ Potree.Viewer.Profile = class ProfileWindow{
 };
 
 // http://epsg.io/
-proj4.defs("UTM10N", "+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs");
+var proj4.defs("UTM10N", "+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs");
 
 Potree.MapView = class{
 		
